e7cc13fc8903c5de977944becfc408b6ea20fd4b
liviabelirocha <liviabelirocha@outlook.com>
2021-12-13 21:10:39 -0300

    feat: saving user info on cookies

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
 import { createContext, ReactNode, useState } from "react";
+import { setCookie } from "nookies";
+
 import { api } from "../services/api";
 
 type User = {
@@ -35,7 +37,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
     try {
       const response = await api.post("sessions", { email, password });
 
-      const { permissions, roles } = response.data;
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, "nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
 
       setUser({
         email,
